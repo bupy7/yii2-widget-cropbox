8dd8694818164c855ad2744657d20e9d7589426f
Pablo Nuñez <mail@pablonunez.com>
2016-09-27 19:25:12 -0300

    Added Spanish translation

---
+++ messages/es/core.php
@@ -0,0 +1,9 @@
+<?php
+return [
+    'Crop' => 'Recortar',
+    'Browse' => 'Seleccionar archivo',
+    'Show original' => 'Mostrar original',
+    'Height' => 'Alto',
+    'Width' => 'Ancho',
+    'Reset' => 'Reiniciar',
+];
